8e9b54501e4d17760a8dd8589bda48e177cca18a
mpsanchis <33475618+mpsanchis@users.noreply.github.com>
2025-12-01 18:34:03 +0100

    feat: Advent of code 2025 day 1 part 2

--- advent_of_code/aoc2025/src/day1/mod.rs
+++ advent_of_code/aoc2025/src/day1/mod.rs
@@ -2,7 +2,7 @@ mod util;
 
 pub use util::parse_input;
 
-pub fn main() {
+pub fn main_pt1() {
     let mut hits_zero = 0;
     let mut position = 50;
     let input = parse_input();
@@ -20,4 +20,49 @@ pub fn main() {
     }
 
     println!("Hits zero: {}", hits_zero);
+}
+
+
+fn stops_at_or_crosses_zero(position: i16, new_position: i16, rotation: i16) -> bool {
+    if new_position == 0 {
+        return true;
+    }
+    if position == 0 {
+        return false;
+    }
+
+    if rotation < 0 {
+        new_position > position
+    } else {
+        new_position < position
+    }
+}
+
+pub fn main_pt2() {
+    let mut crosses_zero_total = 0;
+    let mut position = 50;
+    let input = parse_input();
+
+    for rotation in input.iter() {
+        println!("===");
+        println!("crosses zero total = {crosses_zero_total}");
+        println!("position = {position}");
+        println!("rotation = {rotation}");
+        let destination = position + rotation;
+        let new_position = destination.rem_euclid(100);
+
+        // Calculate number of zero crossings
+        let min_loops = rotation.abs().div_euclid(100);
+        let mut crosses_zero = min_loops;
+        crosses_zero += stops_at_or_crosses_zero(position, new_position, *rotation) as i16;
+
+        position = new_position;
+        println!("=");
+        println!("new position = {destination} =eq= {position}");
+        crosses_zero_total += crosses_zero;
+        println!("crosses zero = {crosses_zero}");
+        println!("crosses zero total = {crosses_zero_total}");
+    }
+
+    println!("Crosses zero: {}", crosses_zero_total);
 }
--- advent_of_code/aoc2025/src/main.rs
+++ advent_of_code/aoc2025/src/main.rs
@@ -1,5 +1,6 @@
 mod day1;
 
 fn main() {
-    day1::main();
+    //day1::main_pt1();
+    day1::main_pt2();
 }

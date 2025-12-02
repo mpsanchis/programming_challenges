ee8c3b91fdb501573151b7d6d446f34089eb6d38
mpsanchis <33475618+mpsanchis@users.noreply.github.com>
2025-12-02 17:38:51 +0100

    fix(aoc2025): Fix day2 part2 with a different algorithm

--- advent_of_code/aoc2025/src/day2/validation.rs
+++ advent_of_code/aoc2025/src/day2/validation.rs
@@ -11,37 +11,48 @@ pub fn is_invalid_id(id_: u64) -> bool {
     half_1 == half_2
 }
 
+pub fn pattern_window_lengths(id_: u64) -> Vec<usize> {
+    let mut window_lengths = Vec::new();
+    let id = id_.to_string();
+
+    for window_length in 1..=id.len()/2 {
+        if id.len() % window_length == 0 {
+            window_lengths.push(window_length);
+        }
+    }
+    window_lengths
+}
+
+fn id_matches_pattern(id: &String, pattern_window_length: &usize) -> bool {
+    let logger = logger();
+
+    let pattern_window = id.get(0..*pattern_window_length).unwrap();
+    logger.log(&format!("Pattern window: {}", pattern_window));
+
+    for step in 0..(id.len() / pattern_window_length) {
+        let id_slice = id.get(step * pattern_window_length..(step + 1) * pattern_window_length).unwrap();
+        logger.log(&format!("ID slice: {}", id_slice));
+        if id_slice != pattern_window {
+            logger.log(&format!("ID slice does not match pattern window"));
+            return false;
+        }
+    }
+    true
+}
+
 pub fn is_invalid_id_2(id_: u64) -> bool {
     let logger = logger();
 
     let id = id_.to_string();
     logger.log(&format!("---\nID: {}\n---", id));
 
-    let mut pattern = id.as_bytes().get(0..1).unwrap();
+    let pattern_window_lengths = pattern_window_lengths(id_);
+    logger.log(&format!("Pattern window lengths: {:?}", pattern_window_lengths));
 
-    for (index, byte) in id.as_bytes().iter().enumerate() {
-        // logger.log(&format!("-\nIndex: {}, Digit: {}", index, *byte as char));
-        // logger.log(&format!("Pattern: {} ({})", String::from_utf8(Vec::from(pattern)).unwrap(), pattern.len()));
-        // check if pattern got too big and finish early
-        if pattern.len() > id.len() / 2 {
-            logger.log("Pattern too big. No pattern found");
-            return false;
-        }
-
-        if pattern[index % pattern.len()] == *byte {
-            // We match the pattern, so continue with the same pattern
-            // logger.log(&format!("Matched pattern: {}", String::from_utf8(Vec::from(pattern)).unwrap()));
-            continue;
-        } else {
-            // If we reached the end, we're done: no repeated pattern found
-            if index == id.len() - 1 {
-                // logger.log("Reached the end. No pattern found");
-                return false;
-            }
-            // If number is not in pattern, then we should increase the pattern window
-            pattern = id.as_bytes().get(0..index+1).unwrap();
-            // logger.log(&format!("Increased pattern to: {}", String::from_utf8(Vec::from(pattern)).unwrap()));
+    for pattern_window_length in pattern_window_lengths.iter() {
+        if id_matches_pattern(&id, pattern_window_length) {
+            return true;
         }
     }
-    true
+    false
 }

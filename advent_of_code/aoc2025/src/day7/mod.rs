1e861c1218f993885e19c9be1a570cd77398293c
mpsanchis <33475618+mpsanchis@users.noreply.github.com>
2025-12-08 08:37:31 +0100

    feat(aoc2025): Day 7 part 2

--- advent_of_code/aoc2025/src/day7/mod.rs
+++ advent_of_code/aoc2025/src/day7/mod.rs
@@ -1,12 +1,13 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 use crate::util::{Part, logger};
 
 mod input;
 mod types;
 
-pub fn main(part: &Part) {
-    let splitters_per_line = input::parse_input();
+use types::{Beam, SplitterPositions};
+
+fn main_1(splitters_per_line: Vec<SplitterPositions>) {
     let mut beam_positions: HashSet<usize> = HashSet::new();
     beam_positions.insert(splitters_per_line[0][0]);
     
@@ -23,4 +24,35 @@ pub fn main(part: &Part) {
         }
     }
     println!("Total splits: {number_of_splits}");
+}
+
+fn main_2(splitters_per_line: Vec<SplitterPositions>) {
+    let mut beams: HashMap<
+        usize, // key: position of the beam
+        usize // value: weight of the beam (how many paths lead to it)
+    > = HashMap::new();
+    beams.insert(splitters_per_line[0][0], 1);
+    
+    for splitters in &splitters_per_line[1..] {
+        logger().logn(&format!("Splitters: {:?}", splitters));
+        for splitter in splitters {
+            if let Some(beam) = beams.remove(splitter) {
+                let prev_beam = *beams.get(&(splitter - 1)).unwrap_or(&0);
+                let next_beam = *beams.get(&(splitter + 1)).unwrap_or(&0);
+                
+                beams.insert(splitter - 1, beam + prev_beam);
+                beams.insert(splitter + 1, beam + next_beam);
+            }
+        }
+    }
+    let paths: usize = beams.iter().map(|beam| beam.1).sum();
+    println!("Total paths: {paths}");
+}
+
+pub fn main(part: &Part) {
+    let splitters_per_line = input::parse_input();
+    match part {
+        Part::One => main_1(splitters_per_line),
+        Part::Two => main_2(splitters_per_line),
+    }
 }
--- advent_of_code/aoc2025/src/day7/types.rs
+++ advent_of_code/aoc2025/src/day7/types.rs
@@ -1,1 +1,35 @@
+use std::hash::{Hash, Hasher};
+
 pub type SplitterPositions = Vec<usize>;
+
+pub struct Beam {
+    pub position: usize,
+    pub weight: usize,
+}
+
+impl Beam {
+    pub fn merge_with(self, other: Option<&Beam>) -> Self {
+        match other {
+            None => self,
+            Some(other_beam) => {
+                Beam { position: self.position, weight: self.weight + other_beam.weight }
+            }
+        }
+    }
+}
+
+impl Hash for Beam {
+    // Use the same criterion as Eq, to be used in a HashSet
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.position.hash(state);
+    }
+}
+
+impl Eq for Beam {}
+
+impl PartialEq for Beam {
+    // Two beams are equal if their positions are equal
+    fn eq(&self, other: &Self) -> bool {
+        self.position == other.position
+    }
+}
